b7077a93f4d4594a2fdcb5daf55ad8a1fe913b8b
Hamza <hamzaoutdoors@gmail.com>
2021-08-09 22:34:02 +0100

    Add local storage functionnality

--- JS/main.js
+++ JS/main.js
@@ -4,26 +4,56 @@ let book = {};
 const addBookButton = document.getElementById('submitButton');
 const booksList = document.getElementById('booksList');
 
-addBookButton.addEventListener('click', addBook)
+addBookButton.addEventListener('click', addBook);
 
-function addBook(title, author){
+function addBook(title, author) {
   // let title = document.querySelector("#title").value;
   // let author = document.querySelector("#author").value;
-  book = {title, author};
+  book = { title, author };
   books.push(book);
 }
 
-function removeBook(title, author){
-  books = books.filter(book => book.title != title && book.author != author)
-  return books
+function removeBook(title, author) {
+  books = books.filter((book) => book.title !== title && book.author !== author);
+  return books;
 }
 
-addBook("title1", "author1")
-addBook("title2", "author2")
-addBook("title3", "author3")
+addBook('title1', 'author1');
+addBook('title2', 'author2');
+addBook('title3', 'author3');
+addBook('title4', 'author4');
+addBook('title5', 'author5');
 
-books.forEach( book => {
-  let li = document.createElement('li');
-  booksList.appendChild(li);
-  li.textContent = book.title + book.author;
-})
+// removeBook('title4', 'author4');
+function displayBooks() {
+  books.forEach((book) => {
+    booksList.innerHTML += '<li>'
+    + `<h3>${book.title}</h3>`
+    + `<p>${book.author}</p>`
+    + '</li>';
+    document.querySelector('.booksContainer').appendChild(booksList);
+  });
+}
+displayBooks();
+
+// Local Storage
+
+function isLocalStorageAvailable() {
+  try {
+    const valueToStore = 'test';
+    const mykey = 'key';
+    localStorage.setItem(mykey, valueToStore);
+    const recoveredValue = localStorage.getItem(mykey);
+    localStorage.removeItem(mykey);
+
+    return recoveredValue === valueToStore;
+  } catch (e) {
+    return false;
+  }
+}
+const form = document.querySelector('#book-form');
+const storeBooks = () => {
+  if (isLocalStorageAvailable()) {
+    localStorage.setItem('booksData', JSON.stringify(books));
+  }
+};
