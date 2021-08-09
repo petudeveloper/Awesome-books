747e4f1bc06333831152c8c34544c0c74eb17159
Hamza <hamzaoutdoors@gmail.com>
2021-08-09 20:08:00 +0100

    add linters

---
+++ JS/main.js
@@ -0,0 +1,46 @@
+const books = [
+  {
+    bookName: 'Politics of Opportunism',
+    authorName: 'R P N Singh',
+  },
+  {
+    bookName: 'Unfinished',
+    authorName: 'Priyanka Chopra Jonas',
+  },
+  {
+    bookName: 'The Daughter’s Tale',
+    authorName: 'Armando Lucas Correa',
+  },
+  {
+    bookName: 'Himself',
+    authorName: 'Jess Kid',
+  },
+  {
+    bookName: 'All the Missing Girls',
+    authorName: 'Megan Miranda',
+  },
+  {
+    bookName: 'he Beautiful Bureaucrat',
+    authorName: 'Helen Phillips',
+  },
+  {
+    bookName: 'The Winemaker’s Wife',
+    authorName: 'Kristin Harmel',
+  },
+  {
+    bookName: 'Love Story',
+    authorName: 'Karen Kingsbury',
+  },
+  {
+    bookName: 'The Dinner List',
+    authorName: 'Rebecca Serle',
+  },
+  {
+    bookName: 'Normal People',
+    authorName: 'Sally Rooney',
+  },
+  {
+    bookName: 'The Family Upstairs',
+    authorName: 'Lisa Jewell',
+  },
+];
