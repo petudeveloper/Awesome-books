e0e06c61637d1cd462dea8ea2971eb97b8a84fbc
petumazo <davidalvarezmazzo@gmail.com>
2021-08-09 15:11:25 -0500

    Add addBook and removeBook functions

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,18 @@
-const books = [
-  {
-    bookName: 'Politics of Opportunism',
-    authorName: 'R P N Singh',
-  },
-  {
-    bookName: 'Unfinished',
-    authorName: 'Priyanka Chopra Jonas',
-  },
-  {
-    bookName: 'The Daughter’s Tale',
-    authorName: 'Armando Lucas Correa',
-  },
-  {
-    bookName: 'Himself',
-    authorName: 'Jess Kid',
-  },
-  {
-    bookName: 'All the Missing Girls',
-    authorName: 'Megan Miranda',
-  },
-  {
-    bookName: 'he Beautiful Bureaucrat',
-    authorName: 'Helen Phillips',
-  },
-  {
-    bookName: 'The Winemaker’s Wife',
-    authorName: 'Kristin Harmel',
-  },
-  {
-    bookName: 'Love Story',
-    authorName: 'Karen Kingsbury',
-  },
-  {
-    bookName: 'The Dinner List',
-    authorName: 'Rebecca Serle',
-  },
-  {
-    bookName: 'Normal People',
-    authorName: 'Sally Rooney',
-  },
-  {
-    bookName: 'The Family Upstairs',
-    authorName: 'Lisa Jewell',
-  },
-];
+let books = [];
+let book = {};
+
+const addBookButton = document.getElementById('submitButton');
+
+addBookButton.addEventListener('click', addBook)
+
+function addBook(title, author){
+  // let title = document.querySelector("#title").value;
+  // let author = document.querySelector("#author").value;
+  book = {title, author};
+  books.push(book);
+}
+
+function removeBook(title, author){
+  books = books.filter(book => book.title != title && book.author != author)
+  return books
+}
