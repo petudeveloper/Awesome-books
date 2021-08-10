72631ecf682df78278c67b1bab934a6523b9f51d
petumazo <davidalvarezmazzo@gmail.com>
2021-08-09 19:02:21 -0500

    Correct linters erros

--- JS/main.js
+++ JS/main.js
@@ -7,40 +7,43 @@ class Book {
   }
 }
 
+function getBooks() {
+  myBooks = JSON.parse(localStorage.myBooks);
+  const books = document.getElementById('books');
+  books.innerHTML = '';
+  let id = 0;
+
+  myBooks.forEach((book) => {
+    books.innerHTML += '<li>'
+    + `<h3>${book.title}</h3>`
+    + `<p>${book.author}</p>`
+    + `<button onClick="destroyBook(${id += 1})">Remove</button>`
+    + '</li>';
+  });
+}
+
 function updateLocalStorage() {
   localStorage.myBooks = JSON.stringify(myBooks);
   getBooks();
 }
 
+function newBook(e) {
+  e.preventDefault();
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  myBooks.push(new Book(title.value, author.value));
+  updateLocalStorage();
+  title.value = '';
+  author.value = '';
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', newBook);
+
+// eslint-disable-next-line no-unused-vars
 function destroyBook(id) {
   myBooks.splice(id, 1);
   updateLocalStorage();
 }
 
-function newBook(e) {
-  e.preventDefault();
-  title = document.getElementById("title");
-  author = document.getElementById("author");
-  myBooks.push(new Book(title.value, author.value));
-  updateLocalStorage();
-  title.value = "";
-  author.value = "";
-}
-const form = document.getElementById("form");
-form.addEventListener("submit", newBook);
-
-function getBooks() {
-  myBooks = JSON.parse(localStorage.myBooks);
-  books = document.getElementById("books");
-  books.innerHTML = "";
-  let id = 0;
-  for (book of myBooks) {
-  books.innerHTML += `
-  <li>
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <button onClick="destroyBook(${id++})">Remove</button>
-  </li>`;
-  }
-}
 if (localStorage.length > 0) getBooks();
